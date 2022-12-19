f4f3aec5d0bae3539f968c1a006746d8d61bcb79
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-12-20 01:04:30 +0200

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - use a pointer to update the value of an integer
+ * @dest: char type string
+ * @src: char type string
+ *
+ * Description Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = -1;
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
+return (dest);
+}
