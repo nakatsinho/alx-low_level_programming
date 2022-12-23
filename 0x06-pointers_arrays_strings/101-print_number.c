d4aa59f94bdb26da026916bebacfb7f07b103297
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-12-23 04:05:47 +0200

    Noise is a buffer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,27 @@
 */
 void print_number(int n)
 {
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+int res, temp, expo;
+expo = 1;
+/*Check negatives*/
+if (n >= 0)
+res = n * -1;
+else
+{
+res = n;
+_putchar('-');
+}
+/*Initialize exponent variable*/
+temp = res;
+while (temp <= -10)
+{
+expo *= 10;
+temp /= 10;
+}
+/*Main */
+while (expo >= 1)
+{
+_putchar(((res / expo) % 10) * -1 + '0');
+expo /= 10;
+}
 }
