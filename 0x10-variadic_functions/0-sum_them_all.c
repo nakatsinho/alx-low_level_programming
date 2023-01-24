7018661b7dbb6e6c40cade760b522a8fa17e8938
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-01-24 10:16:45 +0200

    Fix: Bugs on the first exercise

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,20 +11,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
 
-va_list li;
-int sum = 0;
-unsigned int i;
+	if (n == 0)
+		return (0);
 
-va_start(li, n);
+	va_start(valist, n);
 
-if (n != 0)
-{
-for (i = 0; i < n; i++)
-sum += va_arg(li, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
 }
 
-va_end(li);
-return (sum);
-
-}
